4047531d05e91d13a9fec58a9a22a7e54d3a4506
MaxDBerg <max.dahlberg@gmail.com>
2024-09-29 19:21:54 +0200

    working version of parsing health bars with cv

--- parse-health-bar.py
+++ parse-health-bar.py
@@ -6,6 +6,7 @@ import numpy as np
 import zmq
 import mss
 import math
+import time
 from scipy.signal import savgol_filter
 
 context = zmq.Context()
@@ -15,7 +16,7 @@ socket.bind("tcp://*:5555")
 data_socket = context.socket(zmq.PUSH)
 data_socket.bind("tcp://*:5556")
 
-
+executor = ThreadPoolExecutor(max_workers=5)
 is_running = False
 data_filter_tag = ""
 health_data_buffer = {
@@ -100,11 +101,10 @@ def parse_hp(hp_area):
 def process_bars(bars):
     health_percentages = []
 
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        futures = [executor.submit(parse_hp, bar) for bar in bars]
+    futures = [executor.submit(parse_hp, bar) for bar in bars]
 
-        for future in futures:
-            health_percentages.append(future.result())
+    for future in futures:
+        health_percentages.append(future.result())
 
     return health_percentages
 
@@ -157,6 +157,21 @@ def capture_screen(region=None):
         return img[:, :, :3]
 
 
+def filter_health_data(health_data_buffer):
+    filtered_health_data = []
+    for player in ["player1", "player2", "player3", "player4", "player5"]:
+        if len(health_data_buffer[player]) >= 3:
+            filtered_value = math.ceil(
+                min(savgol_filter(health_data_buffer[player], 3, 1))
+            )
+            filtered_health_data.append(
+                max(filtered_value, 0)
+            )  # Ensure it's non-negative
+        else:
+            filtered_health_data.append(0)  # Default value if not enough data
+    return filtered_health_data
+
+
 def send_health_data(health_bars):
     global health_data_buffer
     global data_filter_tag
@@ -175,86 +190,61 @@ def send_health_data(health_bars):
     for player, new_health in new_health_data.items():
         if new_health == -1:
             new_health = 0
-        if last_data_bundle[player] == -1 and player_dead[player]:
+        if last_data_bundle[player] == 0 and player_dead[player]:
             if data_filter_tag != "recovery_flicker_error":
                 data_filter_tag = "recovery_flicker_error"
                 player_recovery_wait_frames[player] = 0
             if player_recovery_wait_frames[player] == 0:
                 player_recovery_wait_frames[player] = 10
-            else:
-                player_recovery_wait_frames[player] -= 1
-                if player_recovery_wait_frames[player] == 0:
-                    player_dead[player] = False
-                continue
+            player_recovery_wait_frames[player] -= 1
+            if player_recovery_wait_frames[player] == 0:
+                player_dead[player] = False
+            health_data_buffer[player].append(last_data_bundle[player])
+            continue
         if len(health_data_buffer[player]) != 0:
             if health_data_buffer[player][
                 len(health_data_buffer[player]) - 1
-            ] <= 50 and [
-                95,
-                96,
-                97,
-                98,
-                99,
-            ].__contains__(new_health):
+            ] <= 50 and [95, 96, 97, 98, 99].__contains__(new_health):
                 health_data_buffer[player].append(
                     health_data_buffer[player][len(health_data_buffer[player]) - 1]
                 )
                 continue
             else:
                 health_data_buffer[player].append(new_health)
-        elif [
-            95,
-            96,
-            97,
-            98,
-            99,
-        ].__contains__(new_health) and last_data_bundle[player] <= 50:
+        elif [95, 96, 97, 98, 99].__contains__(new_health) and last_data_bundle[
+            player
+        ] <= 50:
             health_data_buffer[player].append(last_data_bundle[player])
         else:
             health_data_buffer[player].append(new_health)
 
     if len(health_data_buffer["player1"]) >= 3:
-        filtered_health_data = []
-        filtered_health_data.append(
-            math.ceil(sum(savgol_filter(health_data_buffer["player1"], 3, 1)) / 3)
-        )
-        filtered_health_data.append(
-            math.ceil(sum(savgol_filter(health_data_buffer["player2"], 3, 1)) / 3)
-        )
-        filtered_health_data.append(
-            math.ceil(sum(savgol_filter(health_data_buffer["player3"], 3, 1)) / 3)
-        )
-        filtered_health_data.append(
-            math.ceil(sum(savgol_filter(health_data_buffer["player4"], 3, 1)) / 3)
-        )
-        filtered_health_data.append(
-            math.ceil(sum(savgol_filter(health_data_buffer["player5"], 3, 1)) / 3)
-        )
+        filtered_health_data = filter_health_data(health_data_buffer)
+        # player1 = math.ceil(min(savgol_filter(health_data_buffer["player1"], 3, 1)))
+        # filtered_health_data.append(player1 if player1 >= 0 else player1 - player1)
+        # player2 = math.ceil(min(savgol_filter(health_data_buffer["player2"], 3, 1)))
+        # filtered_health_data.append(player2 if player2 >= 0 else player2 - player2)
+        # player3 = math.ceil(min(savgol_filter(health_data_buffer["player3"], 3, 1)))
+        # filtered_health_data.append(player3 if player3 >= 0 else player3 - player3)
+        # player4 = math.ceil(min(savgol_filter(health_data_buffer["player4"], 3, 1)))
+        # filtered_health_data.append(player4 if player4 >= 0 else player4 - player4)
+        # player5 = math.ceil(min(savgol_filter(health_data_buffer["player5"], 3, 1)))
+        # filtered_health_data.append(player5 if player5 >= 0 else player5 - player5)
 
         print("\n")
         print(health_data_buffer)
         print("\n")
-        print(savgol_filter(health_data_buffer["player5"], 3, 1))
-        print("\n")
         print(filtered_health_data)
         print("\n")
 
-        if filtered_health_data[0] == 0:
-            player_dead["player1"] = True
-
-        if filtered_health_data[1] == 0:
-            player_dead["player2"] = True
-
-        if filtered_health_data[2] == 0:
-            player_dead["player3"] = True
-
-        if filtered_health_data[3] == 0:
-            player_dead["player4"] = True
-
-        if filtered_health_data[4] == 0:
-            player_dead["player5"] = True
+        for i, player in enumerate(
+            ["player1", "player2", "player3", "player4", "player5"]
+        ):
+            if filtered_health_data[i] == 0:
+                player_dead[player] = True
 
         data_socket.send(bytearray(filtered_health_data))
+        last_data_bundle = dict(zip(new_health_data.keys(), filtered_health_data))
         last_data_bundle = {
             "player1": filtered_health_data[0],
             "player2": filtered_health_data[1],
@@ -262,20 +252,14 @@ def send_health_data(health_bars):
             "player4": filtered_health_data[3],
             "player5": filtered_health_data[4],
         }
-        filtered_health_data = []
-        health_data_buffer = {
-            "player1": [],
-            "player2": [],
-            "player3": [],
-            "player4": [],
-            "player5": [],
-        }
+        for player in health_data_buffer.keys():
+            health_data_buffer[player] = []
 
 
 def handle_null_bar_error(health_bars, null_bar_error, wait_frames_2):
     mutable_health_array = health_bars.copy()
     mutable_health_array.sort()
-    if all(val == -1 for val in mutable_health_array[:3]):
+    if all(val == -1 for val in mutable_health_array[:2]):
         if not null_bar_error and wait_frames_2 == 0:
             print("Setting wait_frames...")
             print("Flagged for possible null bar error!")
@@ -299,6 +283,7 @@ def handle_cv_error(health_bars, standby_process, cv_error, wait_frames):
             return False, True, 5
         elif wait_frames == 0:
             print("Slowing down process cycle...")
+            data_socket.send(bytearray(health_bars))
             return True, False, 0
         elif wait_frames != 0:
             return standby_process, cv_error, wait_frames - 1
@@ -329,6 +314,7 @@ def run_processing_loop():
         try:
             handle_messages()
             if is_running:
+                start_time = time.time()
                 frame = capture_screen(region=party_region)
                 # target_frame = capture_screen(region=healer_target_region)
                 # cv2.imshow("showing frame", target_frame)
@@ -343,9 +329,8 @@ def run_processing_loop():
 
                 if not null_bar_error and not cv_error and not standby_process:
                     send_health_data(health_bar_percentages)
-
-                time.sleep(0.001)
-
+                end_time = time.time()
+                print(f"Slow as shit : {(end_time - start_time)} seconds")
             else:
                 print("Waiting for start signal...")
                 time.sleep(0.5)
